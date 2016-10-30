49b27f1fc9c3719475ac03477345f96ad1bd56dd
mvoevodskiy <mv@aaa0.ru>
2016-10-31 01:13:27 +0300

    PSR-2

--- HW1.php
+++ HW1.php
@@ -12,36 +12,39 @@
 //---------------------------------------------------------------------
 $name = "Ivan";
 $age = 30;
-echo 'Меня зовут ' .$name, '<br>';
-echo 'Мне ' .$age, '<br>';
+echo 'Меня зовут ' . $name, '<br>';
+echo 'Мне ' . $age, '<br>';
 echo '"!|\/\'"\\', '<br>';
 //---------------------------------------------------------------------
 $PictureAll = 80;
-$PictureFeltpen =23;
+$PictureFeltpen = 23;
 $PicturePencil = 40;
-echo $PicturePaint=$PictureAll-$PictureFeltpen-$PicturePencil, '<br>';
+echo $PicturePaint = $PictureAll - $PictureFeltpen - $PicturePencil, '<br>';
 //---------------------------------------------------------------------
 define("LENGTH", 155, true);
-if (defined("LENGTH") == true) echo "Константа LENGTH объявлена!", '<br>';
+if (defined("LENGTH") == true) {
+    echo "Константа LENGTH объявлена!", '<br>';
+}
 echo LENGTH, '<br>';
 //echo LENGTH = 321, '<br>';
 //---------------------------------------------------------------------
 $age = 0;
-if (18 < $age and $age< 65) {
+if (18 < $age and $age < 65) {
     echo "Вам еще работать и работать", '<br>';
-}elseif ( $age> 65) {
+} elseif ($age > 65) {
     echo "Вам   пора   на   пенсию", '<br>';
-}elseif ( $age< 17 and $age> 0){
+} elseif ($age < 17 and $age > 0) {
     echo "Вам   ещё   рано   работать", '<br>';
-}else{
-    echo "Неизвестный   возраст", '<br>';}
+} else {
+    echo "Неизвестный   возраст", '<br>';
+}
 //---------------------------------------------------------------------
 $day = 6;
-switch ($day){
-    case (1 < $day and $day< 5):
+switch ($day) {
+    case (1 < $day and $day < 5):
         echo "Это   рабочий   день", '<br>';
         break;
-    case ( $day==6 or $day== 7):
+    case ($day == 6 or $day == 7):
         echo "Это   выходной   день", '<br>';
         break;
     default:
@@ -52,57 +55,61 @@ switch ($day){
 //---------------------------------------------------------------------
 //
 
-$BMW = array("model" => "X5",
+$BMW = array(
+    "model" => "X5",
     "speed" => "120",
     "doors" => "5",
-    "year" => "2015");
-$Tyota = array("model" => "Camry",
+    "year" => "2015"
+);
+$Tyota = array(
+    "model" => "Camry",
     "speed" => "110",
     "doors" => "3",
-    "year" => "2010");
-$Opel = array("model" => "Astra",
+    "year" => "2010"
+);
+$Opel = array(
+    "model" => "Astra",
     "speed" => "10",
     "doors" => "4",
-    "year" => "2011");
+    "year" => "2011"
+);
 
-$arrGen = array($BMW,$Tyota,$Opel);
+$arrGen = array($BMW, $Tyota, $Opel);
 
 for ($i = 0; $i < count($arrGen); $i++) {
-    echo "CAR".$arrGen[$i], '<br>';
-    echo $arrGen[$i]["model"]." ".$arrGen[$i]["speed"]." ".$arrGen[$i]["doors"]." ".$arrGen[$i]["year"], '<br>';
+    echo "CAR" . $arrGen[$i], '<br>';
+    echo $arrGen[$i]["model"] . " " . $arrGen[$i]["speed"] . " " . $arrGen[$i]["doors"] . " " . $arrGen[$i]["year"], '<br>';
 }
 
 
 //---------------------------------------------------------------------
 
-for ($i =1; $i <=10; $i++) {
-    for ($j =1; $j <=10; $j++) {
-        if ($i%2==0 and $i%2==0) {
-            echo "( ".$i." * ".$j." = ".$i*$j." )", '<br>';
-        }elseif ($i%2<>0 and $j%2<>0){
-            echo "[ ".$i." * ".$j." = ".$i*$j." ]", '<br>';
-        }
-        else{
-            echo $i." * ".$j." = ".$i*$j, '<br>';
+for ($i = 1; $i <= 10; $i++) {
+    for ($j = 1; $j <= 10; $j++) {
+        if ($i % 2 == 0 and $i % 2 == 0) {
+            echo "( " . $i . " * " . $j . " = " . $i * $j . " )", '<br>';
+        } elseif ($i % 2 <> 0 and $j % 2 <> 0) {
+            echo "[ " . $i . " * " . $j . " = " . $i * $j . " ]", '<br>';
+        } else {
+            echo $i . " * " . $j . " = " . $i * $j, '<br>';
         }
     }
 }
 //---------------------------------------------------------------------
-$str= 'test test2 taburetka';
+$str = 'test test2 taburetka';
 echo $str, '<br>';
-$arr = explode(" ",$str);
-for ($i = 0; $i <= count($arr); $i++)
-{
-    echo $arr[$i]." ", '<br>';
+$arr = explode(" ", $str);
+for ($i = 0; $i <= count($arr); $i++) {
+    echo $arr[$i] . " ", '<br>';
 }
-$str ='';
-$i=0;
+$str = '';
+$i = 0;
 while ($i < count($arr)) {
 
-    $str = $str.$arr[$i]."||";
+    $str = $str . $arr[$i] . "||";
     $i++;
 }
-$str=rtrim ( $str,"||" );
+$str = rtrim($str, "||");
 echo $str, '<br>';
 ?>
 </body>
