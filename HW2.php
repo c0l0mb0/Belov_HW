57edbb12e3064c37e9cbef62d979ad7a638946dc
c0l0mb0 <bis80@bk.ru>
2016-11-15 22:20:58 +0500

    отформатировал в соотвествии  PSR-2

--- HW2.php
+++ HW2.php
@@ -13,17 +13,19 @@
 
 //$arr1 = array('banana','apple','car');
 //
-//function StringOpperations($arr1, $param = False)
+//function StringOperations($arr1, $param = false)
 //{
-//    if ($param) return implode("",$arr1);
-//    for ($i = 0; $i <= count($arr1); $i++)
-//    {
-//         echo '<p>'.$arr1[$i].'</p>';
+//    if ($param) {
+//        return implode("", $arr1);
 //    }
+//    return $arr1;
 //}
-//StringOpperations($arr1);
-//$strWithTrue = StringOpperations($arr1, true);
-//echo $strWithTrue, '<br>';
+//StringOperations($arr1);
+//$strWithTrue = StringOperations($arr1, true);
+//for ($i = 0; $i <= count($strWithTrue); $i++) {
+//    echo '<p>'.$strWithTrue[$i].'</p>';
+//}
+
 //------------------------------2---------------------------------------
 //
 //$Arr123 = array(2,0,6,5);
@@ -34,19 +36,18 @@
 //
 //
 //
-//function calc($ArrNumb, $operation){
-//
-//    if (empty($ArrNumb)  ) { // is an array empty
+//function calc($ArrNumb, $operation)
+//{
+//    if (empty($ArrNumb)) { // is an array empty
 //        return 'array error';
 //    }
 //    foreach ($ArrNumb as $value) {//are elements correct
-//           if (strcasecmp(gettype ($value), 'NULL') == 0 or strcasecmp(gettype ($value), 'object') == 0 or strcasecmp(gettype ($value), 'string') == 0 ){
+//           if (strcasecmp(gettype ($value), 'NULL') == 0 or strcasecmp(gettype ($value), 'object') == 0 or strcasecmp(gettype($value), 'string') == 0) {
 //               return 'array element error';
 //           }
 //
 //    }
-//    for ($i = 0; $i < count($ArrNumb)-1; $i++)
-//    {
+//    for ($i = 0; $i < count($ArrNumb)-1; $i++) {
 //        switch ($operation) {
 //            case ('+'):
 //                  $ArrNumb[$i + 1] = $ArrNumb[$i] + $ArrNumb[$i + 1];
@@ -59,10 +60,9 @@
 //                     $ArrNumb[$i + 1] = $ArrNumb[$i] / $ArrNumb[$i + 1];
 //                    break;
 //                }
-//                catch ( Exception $e ){
-//                   echo "Divide by zero!", '<br>';
-//                   return 'error';
-//
+//                catch (Exception $e) {
+//                    echo "Divide by zero!", '<br>';
+//                    return 'error';
 //
 //                }
 //
@@ -78,7 +78,7 @@
 //    return $ArrNumb[count($ArrNumb)-1];//return the last element
 //}
 //
-//$FunResult = calc($Arr123,'/');
+//$FunResult = calc($Arr123, '/');
 //
 //echo $FunResult, '<br>';
 //------------------------------3-----------------------------------------
@@ -87,21 +87,21 @@
 //});
 //
 //
-//function get_sum(){
+//function get_sum()
+//{
 //    $operation = func_get_arg(0);//get first element
 //    $ArrNumb = (func_get_args());//create array
 //    array_shift($ArrNumb);//delete 1 element
-//    if (empty($ArrNumb)  ) { // is an array empty
+//    if (empty($ArrNumb)) { // is an array empty
 //        return 'array error';
 //    }
 //    foreach ($ArrNumb as $value) {//are elements correct
-//           if (strcasecmp(gettype ($value), 'NULL') == 0 or strcasecmp(gettype ($value), 'object') == 0 or strcasecmp(gettype ($value), 'string') == 0 ){
+//           if (strcasecmp(gettype($value), 'NULL') == 0 or strcasecmp(gettype($value), 'object') == 0 or strcasecmp(gettype ($value), 'string') == 0 ){
 //               return 'array element error';
 //           }
 //
 //    }
-//    for ($i = 0; $i < count($ArrNumb)-1; $i++)
-//    {
+//    for ($i = 0; $i < count($ArrNumb)-1; $i++) {
 //        switch ($operation) {
 //            case ('+'):
 //                  $ArrNumb[$i + 1] = $ArrNumb[$i] + $ArrNumb[$i + 1];
@@ -114,9 +114,9 @@
 //                     $ArrNumb[$i + 1] = $ArrNumb[$i] / $ArrNumb[$i + 1];
 //                    break;
 //                }
-//                catch ( Exception $e ){
-//                   echo "Divide by zero!", '<br>';
-//                   return 'error';
+//                catch (Exception $e) {
+//                    echo "Divide by zero!", '<br>';
+//                    return 'error';
 //
 //
 //                }
@@ -132,7 +132,7 @@
 //    }
 //    return $ArrNumb[count($ArrNumb)-1];//return the last element
 //}
-//$FunResult = get_sum('+',2,8,6,5.5);
+//$FunResult = get_sum('+', 2, 8, 6, 5.5);
 //
 //echo $FunResult, '<br>';
 //------------------------------4-----------------------------------------
@@ -147,7 +147,8 @@
 //}
 //
 //
-//function MultipTable($Multiplier1,$Multiplier2){
+//function MultipTable($Multiplier1, $Multiplier2)
+//{
 //    if (CheckVarble($Multiplier1, $Multiplier2)) {//are elements correct
 //        for ($i = 1; $i <= $Multiplier1; $i++) {
 //            for ($j = 1; $j <= $Multiplier2; $j++) {
@@ -162,14 +163,14 @@
 //}
 //
 //
-//$FunResult = MultipTable(2,6);
+//$FunResult = MultipTable(2, 6);
 //
 //echo $FunResult, '<br>';
-//
-//
-//
-//
-//
+
+
+
+
+
 //------------------------------5-----------------------------------------
 //$Word = '  ';
 //
@@ -177,16 +178,14 @@
 //    $ArrWord = str_split($Word);
 //
 //    $counter1 = 0;
-//    for ($i = 0; $i <= (int)((count($ArrWord))/2)-1; $i++){
-//
+//    for ($i = 0; $i <= (int)((count($ArrWord))/2)-1; $i++) {
 //        if ($ArrWord[$i] == $ArrWord[count($ArrWord)-$i-1]) {
 //            $counter1++;
 //        }
 //    }
 //    if (intval((count($ArrWord))/2) == $counter1) {
-//
 //        return 'Палиндром';
-//    }else{
+//    } else {
 //        return 'НЕ палиндром';
 //    }
 //}
@@ -197,21 +196,22 @@
 //echo strtotime("24 February 2016 4 hours 2 minutes"), '<br>';
 ////------------------------------7-----------------------------------------
 //$str='Карл у Клары украл Кораллы';
-//echo str_replace('К','',$str), '<br>';
+//echo str_replace('К', '', $str), '<br>';
 //
 //$str='Две бутылки лимонада';
-//print_r(str_replace('Две','три',$str))
+//print_r(str_replace('Две', 'три', $str));
 ////------------------------------8-----------------------------------------
 //$str1='RX packets:381 errors:0 dropped:0 overruns:)0 frame:0.';
-//function get_RX($str){
+//function get_RX($str)
+//{
 //    if (strpos($str, ':)') !== false) {
 //        return  draw_smile();
 //    }
-//    $ArrStr = explode(" ",$str);
-//    $str = str_replace("packets:","",$ArrStr[1]);
+//    $ArrStr = explode(" ", $str);
+//    $str = str_replace("packets:", "", $ArrStr[1]);
 //    if ($str>1000) {
 //        return "сеть есть";
-//    }else{
+//    } else {
 //        return 'сети нет';
 //    }
 //}
